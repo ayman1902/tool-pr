c15e77f068d62610d159db780aa111a30f0f8bb5
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2019-08-01 00:27:43 +0100

    Update tool-pr.py

--- tool-pr.py
+++ tool-pr.py
@@ -1,8 +1,7 @@
 if __name__ == '__main__':
     import sys
     import webbrowser
-    print('''
-   \033[1;33m###    ##    ## ##     ##    ###    ##    ## 
+    print('''   \033[1;33m###    ##    ## ##     ##    ###    ##    ## 
   ## ##    ##  ##  ###   ###   ## ##   ###   ## 
  ##   ##    ####   #### ####  ##   ##  ####  ## 
 ##     ##    ##    ## ### ## ##     ## ## ## ## 
@@ -17,11 +16,14 @@ if __name__ == '__main__':
 ##     ## ##  ##  ## ##     ## 
 ##     ## ##  ##  ## ##     ## 
 ########   ###  ###  ##     ## \033[34;1m[0]\033[1;32m:\033[35;1m EXIT
+\033[1;33m[\033[34;1m*\033[1;33m]\033[1;31mWelcom on\033[35;1m{\033[1;32mtool-pr\033[35;1m}\033[1;31m Programmable by[\033[34;1mAyman Belhaj\033[1;31m]
 \033[34;1m[10]\033[1;32m:\033[35;1m Follow my account {\033[1;31mfacebook\033[35;1m}
 \033[34;1m[20]\033[1;32m:\033[35;1m Follow my account on {\033[1;32mgithub\033[35;1m}
+\033[34;1m[30]\033[1;32m:\033[35;1m Follow my account on {\033[1;33minstagram\033[35;1m}
+\033[34;1m[\033[1;32m7777\033[34;1m]\033[1;32m:\033[35;1m Follow The officiel page of\033[1;31m DEEP WEB MAROC\033[35;1m in fb
 \033[1;36m|==============================================\033[1;36m| 
-\033[1;36m|    \033[1;32m check it my facebook :::: \033[35;1m Ayman Belhaj \033[1;36m | 
-\033[1;36m|  \033[34;1m   [ DWM FOR LIFE ]  [ HK-G ]    [ TM78 ] \033[34;1m \033[1;36m |  
+\033[1;36m|  \033[1;33m       thanks for the best groups:          \033[1;36m| 
+\033[1;36m|  \033[34;1m [ DWM +18 ] [ HK-G ] [ TM78 ] [MCT] [MKT] \033[1;36m |  
 \033[1;36m|==============================================| ''')
     print("\033[1;31mthis tool it's for the people who want to\033[1;31m ")
     print("\033[1;31mget closer to their goals and see their future\033[1;31m")
@@ -29,7 +31,7 @@ if __name__ == '__main__':
     print("\033[34;1m[1]\033[1;32m:\033[35;1mSMB     \033[34;1m[3]\033[1;32m:\033[35;1mPC")
     print("\033[34;1m[2]\033[1;32m:\033[35;1mSMA     \033[34;1m[4]\033[1;32m:\033[35;1mSVT")
     print("\033[34;1m[5]\033[1;32m:\033[35;1mI am in 2bac")
-    x = int(input("choose your branch:"))
+    x = int(input("choose ==>"))
     #my accounts
     def open_my_facebook_account():
         webbrowser.open_new("https://www.facebook.com/ayman.belhaj.10")
@@ -37,10 +39,20 @@ if __name__ == '__main__':
     def open_my_github_account():
         webbrowser.open_new("https://github.com/ayman1902")
         print("\033[1;31mthanks for visiting my github account \n\033[1;32mGood Bye\033[1;32m")
+    def open_my_instagram_account():
+        webbrowser.open_new("https://www.instagram.com/ayman.bell.10/")
+        print("\033[1;31mthanks for visiting my instagram account \n\033[1;32mGood Bye\033[1;32m")
+    def open_the_officiel_page_of_DWM_in_fb():
+        webbrowser.open_new("https://www.facebook.com/dwebmaroc/")
+        print("\033[1;31mthanks for visiting the officiel page of DWM in fb \n\033[1;32mGood Bye\033[1;32m")
     if x == 10:
         open_my_facebook_account()
     if x == 20:
         open_my_github_account()
+    if x == 30:
+        open_my_instagram_account()
+    if x == 7777:
+        open_the_officiel_page_of_DWM_in_fb()
     #2BAC éxaman nationnal
     if x == 5:
         moyen_de_rejional0 = float(input("your note of rejionnal exam:"))
@@ -48,18 +60,13 @@ if __name__ == '__main__':
         print("\033[1;32mnow you are in 2bac\033[1;32m")
         print("\033[34;1m[1]\033[1;32m:\033[35;1mSMB     \033[34;1m[3]\033[1;32m:\033[35;1mPC")
         print("\033[34;1m[2]\033[1;32m:\033[35;1mSMA     \033[34;1m[4]\033[1;32m:\033[35;1mSVT")
-        y = int(input("choose your branche:"))
-        if y == 10:
-            open_my_facebook_account()
-        if y == 20:
-            open_my_github_account()
+        y = int(input("choose ==>"))
         if y == 0:
             print("\033[1;36mError please retry the process")
             sys.exit()
-        if y != 0 and y != 1 and y != 2 and y != 3 and y != 4  and y != 10 and y != 20 :
+        if y != 0 and y != 1 and y != 2 and y != 3 and y != 4 :
             print("\033[1;36mError please retry the process")
             sys.exit()
-            print("088\[1;29m")
         else:
             print('''\033[34;1m=============================================== \033[1;36m''')
         if y == 1:
@@ -239,10 +246,10 @@ if __name__ == '__main__':
     elif x == 0:
         print("\033[1;36mGood Bye")
         sys.exit()
-    if x!= 0 and x != 1 and x != 2 and x != 3 and x != 4 and x != 5 and x != 10 and x != 20 :
+    if x!= 0 and x != 1 and x != 2 and x != 3 and x != 4 and x != 5 and x != 10 and x != 20 and x != 30 and x != 7777:
         print("\033[1;36mError please retry the process")
         sys.exit()
-         # 1bac éxaman réjonnal
+        # 1bac éxaman réjonnal
     elif x == 1 or x == 2 or x == 3 or x == 4:
      print('''\033[34;1m=============================================== \033[1;36m''')
      francais = float(input("note de francais:")) * 2
